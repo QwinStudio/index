931f0568e26e0089d2c3eaab274c5723f1b83d74
QwinStudio <145882313+QwinStudio@users.noreply.github.com>
2024-01-01 14:08:07 +0800

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -45,7 +45,7 @@ export default function Home() {
             </span>
           </h2>
           <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            这是一个Gebit Qwin的Hexo博客
+            这是一个Gebit Qwin的杂记
           </p>
         </a>
 
