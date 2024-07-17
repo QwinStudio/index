1ff8bdf0832216d200a0796ee923c644cd1d9fb6
QwinStudio <145882313+QwinStudio@users.noreply.github.com>
2024-07-17 14:15:07 +0800

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -1,12 +1,12 @@
 import Image from 'next/image'
 
-export default function Home() {
+export default function 主页() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
       <div className="z-10 max-w-5xl w-full items-center justify-between font-mono text-sm lg:flex">
         <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-          Gebit Qwin
-          <code className="font-mono font-bold">|Home</code>
+          Cyclone
+          <code className="font-mono font-bold"> X | Home</code>
         </p>
         <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:h-auto lg:w-auto lg:bg-none">
           <a
@@ -15,7 +15,7 @@ export default function Home() {
             target="_blank"
             rel="noopener noreferrer"
           >
-            By Cyclone Studio
+            Powered by Gebit Qwin.
           </a>
         </div>
       </div>
@@ -25,78 +25,78 @@ export default function Home() {
           className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
           src="/gbq.png"
           alt="Gebit Qwin Logo"
-          width={180}
-          height={37}
+          width={360}
+          height={80}
           priority
         />
       </div>
 
       <div className="mb-32 grid text-center lg:max-w-5xl lg:w-full lg:mb-0 lg:grid-cols-4 lg:text-left">
         <a
-          href="https://blog.gebitqwin.com"
+          href="https://gebitqwin.com"
           className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
           target="_blank"
           rel="noopener noreferrer"
         >
           <h2 className={`mb-3 text-2xl font-semibold`}>
-            Blog{' '}
+            文章{' '}
             <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
               -&gt;
             </span>
           </h2>
           <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            这是一个Gebit Qwin的杂记
+            Gebit Qwin's
           </p>
         </a>
 
         <a
-          href="https://music.gebitqwin.com"
+          href="https://docs.cyclonex.org"
           className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
           target="_blank"
           rel="noopener noreferrer"
         >
           <h2 className={`mb-3 text-2xl font-semibold`}>
-            Music{' '}
+            文档{' '}
             <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
               -&gt;
             </span>
           </h2>
           <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            在Funny Add中享受音乐的乐趣
+            提供技术支持
           </p>
         </a>
 
         <a
-          href="https://gemini.gebitqwin.com"
+          href="https://occasionally.cyclonex.org/"
           className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
           target="_blank"
           rel="noopener noreferrer"
         >
           <h2 className={`mb-3 text-2xl font-semibold`}>
-            AI{' '}
+            博客{' '}
             <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
               -&gt;
             </span>
           </h2>
           <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Gemini Pro AI
+            Rieder Fake's
           </p>
         </a>
 
         <a
-          href="https://start.gebitqwin.com"
+          href="https://pxclub.cyclonex.org"
           className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
           target="_blank"
           rel="noopener noreferrer"
         >
           <h2 className={`mb-3 text-2xl font-semibold`}>
-            Start{' '}
+            论坛{' '}
             <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
               -&gt;
             </span>
           </h2>
           <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            一个简单的搜索开始页
+            Pxclub 摄影论坛
           </p>
         </a>
       </div>
