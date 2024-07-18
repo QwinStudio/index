3976c5bc21b07d00a48312a3b4abd728bccb3822
QwinStudio <145882313+QwinStudio@users.noreply.github.com>
2024-07-18 21:06:45 +0800

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -5,8 +5,8 @@ export default function 主页() {
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
       <div className="z-10 max-w-5xl w-full items-center justify-between font-mono text-sm lg:flex">
         <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-          Cyclone
-          <code className="font-mono font-bold"> X | Home</code>
+          Cyclone X
+          <code className="font-mono font-bold">|Home</code>
         </p>
         <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:h-auto lg:w-auto lg:bg-none">
           <a
@@ -79,7 +79,7 @@ export default function 主页() {
             </span>
           </h2>
           <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            一点灵感 我们的小站&apos;
+            一点灵感 我们的小站
           </p>
         </a>
 
