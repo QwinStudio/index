8ae3b63060fad5c4b9aca925337fff280dbc09af
QwinStudio <145882313+QwinStudio@users.noreply.github.com>
2025-11-08 20:27:31 +0800

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -5,7 +5,7 @@ import './globals.css'
 const inter = Inter({ subsets: ['latin'] })
 
 export const metadata: Metadata = {
-  title: 'Cyclone X | Home',
+  title: '极屿Gebit的个人主页',
   description: 'GEQ',
 }
 
